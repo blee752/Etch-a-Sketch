23bda853cbe9e6ff7e3e12702ca0499d586351e4
Brian <Blee752@mtroyal.ca>
2021-11-23 21:13:33 -0700

    Add title of project to the page, buttons to clear and change size of grid

--- Scripts.js
+++ Scripts.js
@@ -4,19 +4,17 @@ const container = document.getElementById('main');
 
 function makeGrid (rows, cols) {
 
-    console.log(container);
     container.style.setProperty('--rows', rows);
     container.style.setProperty('--cols', cols);
-
- /*    container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-    container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`; */
     for(i=0; i < rows * cols; i++) {
         let cell = document.createElement('div');
         container.appendChild(cell).className = 'grid-item';
     }
 }
 
-makeGrid(16,16);
+let rows = getComputedStyle(document.documentElement).getPropertyValue('--rows');
+let cols = getComputedStyle(document.documentElement).getPropertyValue('--cols');
+makeGrid(rows,cols);
 
 //first idea 
 /* 
@@ -31,16 +29,33 @@ makeGrid(16,16);
     lets go with second idea first
 */
 
-let divEffect = document.querySelector('div');
-
+const divEffect = document.querySelector('.grid');
 divEffect.addEventListener('mouseover', (e) => {
     if(e.target.className === 'grid-item') {
         e.target.style.backgroundColor = 'black';
     };
-    })
+    });
 
-divEffect.addEventListener('mouseout', (e) => {
-    if(e.target.className === 'grid-item') {
-        e.target.style.backgroundColor = 'white';
-    };
-})
+const clearEffect = document.querySelector('.clear');
+clearEffect.addEventListener('click', (e) => {
+    clearGrid();
+});
+
+function clearGrid () {
+    const divsToClear = document.getElementById('main');
+    divsToClear.replaceChildren();
+    /* while(divsToClear.firstChild) {
+        divsToClear.removeChild(divsToClear.firstChild);
+    } */
+    makeGrid(rows,cols); 
+}
+
+
+const changeGrid = document.querySelector('.Change-Size');
+changeGrid.addEventListener('click', (e) => {
+    let size = prompt('Please enter the new grid size:');
+    size = parseInt(size);
+    rows = size;
+    cols = size;
+    clearGrid();
+});
